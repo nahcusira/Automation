4291c6bb6ac6e279d99219db8cb8c4c0174bf86e
kami sama <87233531+nahcusira@users.noreply.github.com>
2024-01-02 08:46:53 +0700

    Update TAPS.ps1

--- TAPS.ps1
+++ TAPS.ps1
@@ -1,13 +1,3 @@
-# Step 1: Connect Azure CLI
-
-# az login --tenant "Your tenant"
-
-# Step 2: Connect AzureAD
-
-# Connect-AzureAD -TenantId "Your tenant"
-
-# Step 3: .\Automation.ps1
-
 terraform init
 terraform plan -out=tfplan
 terraform apply tfplan
